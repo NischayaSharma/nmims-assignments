4affd6357892bdccbbf1cff1b24bfe5dce6ad4f9
NischayaSharma <nischaya1703@gmail.com>
2019-09-03 10:16:11 +0530

    fix: Adding Q2 in Assignment-1

--- sem-3/OopAssignments/Assignment-1/Ques9.java
+++ sem-3/OopAssignments/Assignment-1/Ques9.java
@@ -14,23 +14,20 @@ class Ques9 {
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter: \n1. Volume of cylinder\n2. Volume of Cube\n3. Volume of Cuboid");
-        int choice = in.nextInt();
-        switch(choice){
-            case 1:
+        switch(args[0]){
+            case "1":
                 System.out.print("Enter the radius: ");
                 double radius = in.nextDouble();
                 System.out.print("Enter the height: ");
                 double height = in.nextDouble();
                 System.out.println(volume(radius, height));
                 break;
-            case 2:
+            case "2":
                 System.out.print("Enter the side: ");
                 double side = in.nextDouble();
                 System.out.println(volume(side));
                 break;
-            case 3:
+            case "3":
                 System.out.print("Enter the length: ");
                 double length = in.nextDouble();
                 System.out.print("Enter the breadth: ");
