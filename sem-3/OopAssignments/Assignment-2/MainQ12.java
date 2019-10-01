a80bff77e5b1bc66dd63e9b927877caa700d3c9c
NischayaSharma <nischaya1703@gmail.com>
2019-10-01 12:24:18 +0530

    fix: Added assgmt2.Q12

---
+++ sem-3/OopAssignments/Assignment-2/MainQ12.java
@@ -0,0 +1,13 @@
+package assgmt2.Q12;
+
+import java.util.*;
+public class MainQ12 {
+  public static void main(String[] args) {
+    ArrayList<String> shoppingList = new ArrayList<String>();
+    shoppingList.add("Milk");
+    shoppingList.add("Cheese");
+    shoppingList.add("Carrot");
+    shoppingList.add("Rice");
+    System.out.println(shoppingList);
+  }
+}
